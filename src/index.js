b530e5ced99aa83a56d1be8cfdb0b83f5fe831d2
claudiogonzalez <claudio@simtlix.com>
2018-04-22 23:03:18 -0300

    Importamos SearchBar como una file reference

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyBIY-ihd0-6uto9hYThxxCXvpHVzQOGyEs";
 
 const App = () => {
 	return <div>Hola</div>;
+}
+
+class App extends Component{
+
 }
 
 ReactDOM.render(new App(), document.querySelector(".container"));
