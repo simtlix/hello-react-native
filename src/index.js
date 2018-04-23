07cad182477985e502935d8cce17a6978d62ffae
claudiogonzalez <claudio@simtlix.com>
2018-04-22 22:34:57 -0300

    Creamos un nuevo componente para Buscar videos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = "AIzaSyBIY-ihd0-6uto9hYThxxCXvpHVzQOGyEs";
+
 const App = () => {
 	return <div>Hola</div>;
 }
 
 ReactDOM.render(new App(), document.querySelector(".container"));
 //ReactDOM.render(<App/>, document.querySelector(".container"));
+
